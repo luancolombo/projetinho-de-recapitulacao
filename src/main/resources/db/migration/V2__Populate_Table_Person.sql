83817f1581eda2b570cd508b05f8bd20249feac6
Luan Colombo <luanbelly@gmail.com>
2026-04-28 17:37:22 +0100

    migrations com flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,12 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+                                                                                (1, 'Quarteira', 'Luan', 'Male', 'Colombo'),
+                                                                                (2, 'Quarteira', 'Gabriela', 'Female', 'Hoffer'),
+                                                                                (3, 'Faro', 'Victor', 'Male', 'Carpes'),
+                                                                                (4, 'Quarteira', 'Bianca', 'Female', 'Hoffer'),
+                                                                                (7, 'Quarteira', 'Rogerio', 'Male', 'Datena'),
+                                                                                (8, 'Criciuma', 'Leonina', 'Female', 'Macedo'),
+                                                                                (9, 'Criciuma', 'Sonia', 'Female', 'Macedo'),
+                                                                                (11, 'Rio de Janeiro  RJ', 'Roberto', 'Male', 'Marinho'),
+                                                                                (12, 'Ohaio-USA', 'Nicola', 'Male', 'Tesla');
+
+
